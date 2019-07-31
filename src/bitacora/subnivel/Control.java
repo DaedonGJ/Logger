b0252fa214d18070dcaa32e724bd6ec180c444d4
Carlos Garrido Junco <str_gjc@stratesys.es>
2019-07-31 16:21:05 +0200

    loggers

---
+++ src/bitacora/subnivel/Control.java
@@ -0,0 +1,10 @@
+package bitacora.subnivel;
+
+import java.util.logging.*;
+public class Control {
+	private final static Logger LOGGER = Logger.getLogger("bitacora.subnivel.Control");
+	
+	 public void controlar(){
+	        LOGGER.log(Level.INFO, "Proceso exitoso");
+	    }
+}
---
+++ src/bitacora/subnivel/Utilidades.java
@@ -0,0 +1,12 @@
+package bitacora.subnivel;
+
+import java.util.logging.*;
+
+public class Utilidades {
+    private final static Logger LOGGER = Logger.getLogger("bitacora.subnivel.Utilidades");
+    
+    public void funcionDudosa(){
+        LOGGER.log(Level.SEVERE, "ERROR MASIVO");
+    }
+
+}
